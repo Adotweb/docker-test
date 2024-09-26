c7ad55130d9876f165b423d213b51f6f6a1e7dde
Alim Weber <alim.weber@outlook.com>
2024-09-26 13:33:39 +0200

    test

--- index.js
+++ index.js
@@ -15,11 +15,15 @@ app.get("/", (req, res)=> {
 app.get("/hello", (req, res)=> {
 
 
+	console.log("hello")
 	res.send("siuuu")
 
 })
 
 app.post("/something", (req, res) => {
+
+	
+
 	res.send(req.body)
 })
 
