8269224fb5cd6078c3affd88604e4b55808a1570
Alim Weber <alim.weber@outlook.com>
2024-09-29 09:54:03 +0200

    some

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require("express");
-
+const http = require("http")
 const app = express()
 const body_parser = require("body-parser")
 const PORT = process.env.PORT || 3000
 
+const ws = require("ws")
+
+const server = http.createServer(app)
+
+const wss = new ws.WebSocketServer({server})
+
+wss.on("connection", socket => {
+
+
+	socket.on("message", msg => {
+
+		console.log(msg.toString())
+
+		socket.send("ping")
+
+	})
+
+})
 
 app.use(express.json())
 app.use(body_parser())
@@ -29,4 +47,4 @@ app.post("/something", (req, res) => {
 	res.send("hello")
 })
 
-app.listen(PORT, () => console.log("server runs on port", PORT))
+server.listen(PORT, () => console.log("server runs on port", PORT))
